5127dd8e28818797c218b26e9d84907d42ef1db8
Hrishikesh Varma <chamarthi.hrishikesh@gmail.com>
2020-06-13 13:10:09 +0530

    intial commit

---
+++ script.py
@@ -0,0 +1,22 @@
+import math
+import os
+import sys
+
+import requests
+
+# print(sys.version)
+# print(sys.executable)
+# print sys.version
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+# print(greet('World'))
+
+r = requests.get("https://www.google.com")
+print(r.status_code)
+
+name = input("name? ")
+print(name)
